ccebe3105cf3ac91252788fce302a71bdb987746
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2023-09-17 12:09:34 +0530

    Time: 0 ms (100%), Space: 40.5 MB (40.49%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int pos=0,prevpos=-1,j=0,reduction=0;
+        for(int i=0;i<s.length();i++){
+            pos=t.indexOf(s.charAt(i));
+            if(pos==-1)
+                return false;
+            else
+            t=t.substring(pos+1,t.length());
+            
+        }
+        return true;
+    }
+}
