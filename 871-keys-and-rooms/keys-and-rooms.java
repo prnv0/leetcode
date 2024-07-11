6704fb922144ba9a2e0636bad9bdcd3ed328ed1a
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-07-11 23:27:08 +0530

    Time: 1 ms (77.68%) | Memory: 44.3 MB (25.33%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         Stack<Integer> stack= new Stack<Integer>();
-        ArrayList<Integer> visited= new ArrayList<Integer>();
+        int[] visited= new int[rooms.size()];
         stack.push(0);
-        visited.add(0);
+        visited[0]=1;
         int count=1;
         while(!stack.isEmpty()){
             for(int i:rooms.get(stack.pop())){
-                if(!visited.contains(i)){
-                    visited.add(i);
+                if(visited[i]!=1){
+                    visited[i]=1;
                     count++;
                     stack.push(i);;
                 }
