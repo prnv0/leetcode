8d31fd02daed1386b97981bb621e5e8638d79182
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-08-29 16:58:53 +0530

    Time: 2 ms (59.42%) | Memory: 44.7 MB (94.34%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,13 +1,22 @@
 class Solution {
     public int jump(int[] nums) {
-        int jumps = 0, curEnd = 0, curFarthest = 0;
-	    for (int i = 0; i < nums.length - 1; i++) {
-		curFarthest = Math.max(curFarthest, i + nums[i]);
-		if (i == curEnd) {
-			jumps++;
-			curEnd = curFarthest;
-		}
-	}
-	return jumps;
+        int ans = 0;
+    int end = 0;
+    int farthest = 0;
+
+    // Implicit BFS
+    for (int i = 0; i < nums.length - 1; ++i) {
+      farthest = Math.max(farthest, i + nums[i]);
+      if (farthest >= nums.length - 1) {
+        ++ans;
+        break;
+      }
+      if (i == end) {   // Visited all the items on the current level
+        ++ans;          // Increment the level
+        end = farthest; // Make the queue size for the next level
+      }
+    }
+
+    return ans;
     }
 }
