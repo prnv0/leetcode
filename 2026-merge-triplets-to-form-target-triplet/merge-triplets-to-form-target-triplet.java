8f30b5d1bcf7057169bcf8b275078db3bd837b9b
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-09-16 17:37:00 +0530

    Time: 6 ms (44.11%) | Memory: 121.2 MB (37.03%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         int x=0,y=0,z=0;
-        List<Integer> list=new ArrayList<>();
         for(int i=0;i<triplets.length;i++){
             if(triplets[i][0]<=target[0]&&triplets[i][1]<=target[1]&&triplets[i][2]<=target[2]){
                 if(triplets[i][0]==target[0])
