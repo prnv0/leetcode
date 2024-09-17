26361cfcca0470baef5270b6003ffe91d20b6004
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-09-17 10:31:45 +0530

    Time: 5 ms (78.33%) | Memory: 41.8 MB (81.94%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,19 +1,22 @@
 class Solution {
-    public List<Integer> partitionLabels(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        char[] ch=s.toCharArray();
-        int end=0;
-        //store last index of each character
-        for(int i=0;i<ch.length;i++){
-            map.put(ch[i],i);
+    public List<Integer> partitionLabels(String S) {
+        if(S == null || S.length() == 0){
+            return null;
         }
-        int endprev=0;
         List<Integer> list = new ArrayList<>();
-        for(int i=0;i<ch.length;i++){
-            end=Math.max(end,map.get(ch[i]));
-            if(i==end){
-                list.add(i+1-endprev);
-                endprev=end+1;
+        int[] map = new int[26];  // record the last index of the each char
+
+        for(int i = 0; i < S.length(); i++){
+            map[S.charAt(i)-'a'] = i;
+        }
+        // record the end index of the current sub string
+        int last = 0;
+        int start = 0;
+        for(int i = 0; i < S.length(); i++){
+            last = Math.max(last, map[S.charAt(i)-'a']);
+            if(last == i){
+                list.add(last - start + 1);
+                start = last + 1;
             }
         }
         return list;
