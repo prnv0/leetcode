bf5784b9e9e6012389d0d5ae2e3dffb137e2f308
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-09-17 11:49:25 +0530

    Time: 1 ms (33.91%) | Memory: 42.1 MB (9.27%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,25 +1,26 @@
 class Solution {
+    private Boolean[][] dp;
     public boolean checkValidString(String s) {
-        Stack<Integer> leftID = new Stack<>();
-        Stack<Integer> starID = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '(')
-                leftID.push(i);
-            else if (ch == '*')
-                starID.push(i);
-            else {
-                if (leftID.isEmpty() && starID.isEmpty())   return false;
-                if (!leftID.isEmpty())
-                    leftID.pop();
-                else 
-                    starID.pop();
+        dp = new Boolean[s.length() + 1][s.length() + 1];
+        return check(s, 0, 0);
+    }
+    private boolean check(String s, int start, int count) {
+        if (count < 0) return false;
+        int y = count;
+        if (dp[start][y] != null) return dp[start][y];
+        for (int i = start; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                if (count <= 0) return false;
+                count--;
+            } else if (c == '*') {
+                dp[start][y] = (check(s, i + 1, count + 1) || check(s, i + 1, count - 1) || check(s, i + 1, count));
+                return dp[start][y];
             }
         }
-        while (!leftID.isEmpty() && !starID.isEmpty()) {
-            if (leftID.pop() > starID.pop()) 
-                return false;
-        }
-        return leftID.isEmpty();
+        dp[start][y] = (count == 0);
+        return dp[start][y];
     }
 }
