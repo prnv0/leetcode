370cdf4717e7143aad7352289f92b8ce5b8909eb
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-09-28 11:36:01 +0530

    Time: 4 ms (100.00%) | Memory: 44.6 MB (75.72%) - LeetSync

--- 859-design-circular-deque/design-circular-deque.java
+++ 859-design-circular-deque/design-circular-deque.java
@@ -1,77 +1,66 @@
 class MyCircularDeque {
-
-   private final int[] buffer;
-   private int head;
-   private int tail;
-   private int size;
-
-   public MyCircularDeque(int k) {
-       this.buffer = new int[k];
-       this.head = 1;
-       this.tail = 0;
-       this.size = 0;
-   }    
-
-   public boolean insertFront(int value) {
-       if (isFull()) {
-           return false;
-       }
-       head = prev(head);
-       buffer[head] = value;
-       size++;
-       return true;
-   }
-
-   private int prev(int i) {
-       return i == 0 ? buffer.length - 1 : --i;
-   }
-   
-   public boolean insertLast(int value) {
-       if (isFull()) {
-           return false;
-       }
-       tail = next(tail);
-       buffer[tail] = value;
-       size++;
-       return true;
-   }
-   
-   private int next(int i) {
-       i++;
-       return i == buffer.length ? 0 : i;
-   }
-
-   public boolean deleteFront() {
-       if (isEmpty()) {
-           return false;
-       }
-       head = next(head);
-       size--;
-       return true;
-   }
-   
-   public boolean deleteLast() {
-       if (isEmpty()) {
-           return false;
-       }
-       tail = prev(tail);
-       size--;
-       return true;
-   }
-   
-   public int getFront() {
-       return isEmpty() ? -1 : buffer[head];
-   }
-   
-   public int getRear() {
-       return isEmpty() ? -1 : buffer[tail];
-   }
-   
-   public boolean isEmpty() {
-       return size == 0;
-   }
-   
-   public boolean isFull() {
-       return size == buffer.length;
-   }
+    private int head, tail, size, n;
+    private final int[] a;
+    public MyCircularDeque(int k) {
+        a = new int[k];
+        n = a.length;
+        tail = 1;
+    }
+    
+    public boolean insertFront(int x) {
+        if (size == n) return false;
+        a[head = ++head % n] = x;
+        size++;
+        return true;
+    }
+    
+    public boolean insertLast(int x) {
+        if (size == n) return false;
+        a[tail = (--tail + n) % n] = x;
+        size++;
+        return true;
+    }
+    
+    public boolean deleteFront() {
+        if (size == 0) return false;
+        head = (--head + n) % n;
+        size--;
+        return true;
+    }
+    
+    public boolean deleteLast() {
+        if (size == 0) return false;
+        tail = ++tail % n;
+        size--;
+        return true;
+    }
+    
+    public int getFront() {
+        return size == 0 ? -1 : a[head];
+    }
+    
+    public int getRear() {
+        return size == 0 ? -1 : a[tail];
+    }
+    
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    public boolean isFull() {
+        return size == a.length;
+    }
 }
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
