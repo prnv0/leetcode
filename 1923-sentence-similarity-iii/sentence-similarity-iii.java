1dc03a11984baa930ba0609c7c068873544c4fdf
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-10-06 11:01:02 +0530

    Time: 3 ms (15.48%) | Memory: 41.8 MB (39.29%) - LeetSync

---
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        if(sentence1.length()<=sentence2.length())
+            return canequal(sentence1,sentence2);
+        else
+            return canequal(sentence2,sentence1);
+    }
+    public boolean canequal(String s1,String s2){
+        String[] arr1= s1.split(" ");
+        String[] arr2= s2.split(" ");
+        int length=0;
+        //check prefix length
+        int i=0;
+        while(i<arr1.length&&arr1[i].equals(arr2[i])){
+            length++;
+            i++;
+        }
+        int x=arr1.length-1;
+        int y=arr2.length-1;
+        //check suffix length
+        // if(length==arr1.length)
+        //     return true;
+        while(x>=0&&length!=arr1.length&&arr1[x].equals(arr2[y])){
+            length++;
+            x--;
+            y--;
+        }
+        System.out.println(length);
+        if(length==arr1.length)
+            return true;
+        else return false;
+
+    }
+}
