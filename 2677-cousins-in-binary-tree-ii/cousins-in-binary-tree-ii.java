a8ef3c2eb0559164ee7ab9a7793a16626e73a825
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-10-23 11:03:55 +0530

    Time: 20 ms (74.58%) | Memory: 76.9 MB (72.03%) - LeetSync

---
+++ 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode replaceValueInTree(TreeNode root) {
+         int[] arr= new int[100001];
+         Queue<TreeNode> q= new LinkedList<>();
+         q.add(root);
+         int height=0;
+         while(!q.isEmpty()){
+            int len=q.size();
+            int sum=0;
+            for(int i=0;i<len;i++){
+                TreeNode temp=q.remove();
+                sum+=temp.val;
+                if(temp.left!=null)
+                    q.add(temp.left);
+                if(temp.right!=null)
+                    q.add(temp.right);
+            }
+            arr[height++]=sum;
+         }
+        arr[height]=0;
+        q.add(root);
+        root.val=0;
+        height=1;
+
+        while(!q.isEmpty()){
+            int len=q.size();
+            for(int i=0;i<len;i++){
+            int sum=arr[height];
+            TreeNode temp=q.remove();
+            if(temp.left!=null)
+                sum=sum-temp.left.val;
+            if(temp.right!=null){
+                //System.out.println(sum+" - "+ temp.right.val);
+                sum=sum-temp.right.val;
+                
+            }
+            
+            if(temp.left!=null){
+                temp.left.val=sum;
+                q.add(temp.left);
+            }
+            if(temp.right!=null){
+                temp.right.val=sum;
+                q.add(temp.right);
+            }
+            }
+            height++;
+
+         }
+        return root;
+    }
+}
